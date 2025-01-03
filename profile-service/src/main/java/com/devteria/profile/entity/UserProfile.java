fe471562fd2c8827bdb10c6911c561f56a5a3133
Phạm Văn Quang <90913961+QuangPV10@users.noreply.github.com>
2025-01-03 17:34:39 +0700

    add neo4j to profile service

---
+++ profile-service/src/main/java/com/devteria/profile/entity/UserProfile.java
@@ -0,0 +1,32 @@
+package com.devteria.profile.entity;
+
+import java.time.LocalDate;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Property;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+// Node() in Neo4j used to define name of table
+@Node("user_profile")
+public class UserProfile {
+    @Id
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    String id;
+    // define name a column
+    @Property("userId")
+    String userId;
+    String firstName;
+    String lastName;
+    LocalDate dob;
+    String city;
+}
---
+++ profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
@@ -0,0 +1,36 @@
+package com.devteria.profile.service;
+
+import com.devteria.profile.dto.request.ProfileCreationRequest;
+import com.devteria.profile.dto.response.UserProfileResponse;
+import com.devteria.profile.entity.UserProfile;
+import com.devteria.profile.mapper.UserProfileMapper;
+import com.devteria.profile.repository.UserProfileRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Slf4j
+public class UserProfileService {
+    UserProfileRepository userProfileRepository;
+    UserProfileMapper userProfileMapper;
+
+    public UserProfileResponse createProfile(ProfileCreationRequest profileCreationRequest) {
+        UserProfile userProfile = userProfileMapper.toUserProfile(profileCreationRequest);
+        userProfileRepository.save(userProfile);
+        return userProfileMapper.toUserProfileResponse(userProfile);
+
+    }
+
+    public UserProfileResponse getProfile(String profileId) {
+        UserProfile userProfile = userProfileRepository.findById(profileId).orElseThrow(() -> new RuntimeException("User Not Found"));
+
+        return userProfileMapper.toUserProfileResponse(userProfile);
+
+    }
+
+}
